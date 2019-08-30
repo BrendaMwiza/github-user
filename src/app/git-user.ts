92198e1ffb29294df4820c8fc0d0b6079eb1cd54
BrendaMwiza <brendabrizy@gmail.com>
2019-08-30 17:28:36 +0200

    add class

--- src/app/git-user.ts
+++ src/app/git-user.ts
@@ -1,5 +1,5 @@
 export class GitUser {
-        constructor(public id: number,public Name: string,public Bio: string, public Repositories: number, public Email: string, public Location: string, public Company: string, public avatar_url: string){
+        constructor(public Name: string,public Bio: string, public Repositories: number, public Email: string, public Location: string, public Company: string, public avatar_url: string){
         }
 }
       
--- src/app/git/git.component.ts
+++ src/app/git/git.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { GitUser } from '../git-user';
 import { GitService } from '../git-service/git.service';
 import { HttpClient } from '@angular/common/http';
-import { stringify } from '@angular/compiler/src/util';
 
 
 @Component({
@@ -14,16 +13,6 @@ export class GitComponent implements OnInit {
 
   user:GitUser;
 
-
-
-  // this.http.get<ApiResponse>("https://api.github.com/users/daneden?access_token=77b86016a5dd86ee5452cfed26fce05c0df89333").subscribe(data=>{
-  //     // Succesful API request
-  //     this.user = new GitUser(data.id, data.Name, data.Bio, data.Repositories, data.Email, data.Location, data.Company, data.avatar_url)
-  //   };
-    // err=>{
-    //     this.user = new GitUser()
-    //     console.log("An error occurred");
-    // })
 
   constructor(gitService:GitService, private http:HttpClient) {
    }
@@ -38,6 +27,12 @@ export class GitComponent implements OnInit {
       Company:string;
       avatar_url:string;
     }
+
+    this.http.get<ApiResponse>("https://api.github.com/users/daneden?access_token=77b86016a5dd86ee5452cfed26fce05c0df89333").subscribe(data=>{
+      // Succesful API request
+      this.user = new GitUser(data.Name, data.Bio, data.Repositories, data.Email, data.Location, data.Company, data.avatar_url)
+    })
   }
 
 }
+
