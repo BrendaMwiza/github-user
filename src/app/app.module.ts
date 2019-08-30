4335073971fe360f38e8ba195858528a52f957e3
BrendaMwiza <brendabrizy@gmail.com>
2019-08-30 10:26:31 +0200

    start push

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
+import { DateCountPipe } from './date-count.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GitComponent,
+    DateCountPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
